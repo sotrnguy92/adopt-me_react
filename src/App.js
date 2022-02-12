9079db8ec10980c21d62fe6e8e5729e14ff92358
sotrnguy92 <sonnguyen092@gmail.com>
2022-02-12 14:51:05 -0800

    adding props to Pet component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement("div", {}, [
-        React.createElement("h2", {}, "Atlas"),
-        React.createElement("h3", {}, "Dog"),
-        React.createElement("h3", {}, "Siberian Huskey")
+        React.createElement("h2", {}, props.name),
+        React.createElement("h3", {}, props.animal),
+        React.createElement("h3", {}, props.breed)
 
     ])
 }
@@ -12,10 +12,10 @@ const App = () => {
         "div",
         {},
         [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet),
-            React.createElement(Pet),
-            React.createElement(Pet)
+            React.createElement("h1", {id:"my-brand"}, "Adopt Me!"),
+            React.createElement(Pet, {name:"Atlas", animal: "Dog", breed: "Siberian Huskey"}),
+            React.createElement(Pet, {name:"Pepper", animal: "Bird", breed: "Cockatiel"}),
+            React.createElement(Pet, {name:"Simba", animal: "Lion", breed: "African Lion"})
         ]
         );
 };

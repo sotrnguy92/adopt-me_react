554e9668b4b5a380f1db67aec06a457640a097b9
sotrnguy92 <sonnguyen092@gmail.com>
2022-02-13 22:42:01 -0800

    enabling eslint and moving Pet component to its own file

---
+++ src/Pet.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
+  ]);
+};

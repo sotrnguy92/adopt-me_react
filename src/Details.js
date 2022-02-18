ac8b714579147ba1290869b2252cc9eebe5ca251
sotrnguy92 <sonnguyen092@gmail.com>
2022-02-17 20:47:57 -0800

    creating a Carousel component to handle displaying images from api

--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,10 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
+import Carousel from "./Carousel";
 
 class Details extends Component {
-  constructor() {
-    super();
 
-    this.state = { loading: true };
-  }
+  state = { loading: true };
 
   async componentDidMount() {
     const res = await fetch(
@@ -25,9 +23,10 @@ class Details extends Component {
 
   render() {
     if(this.state.loading) return <h2>loading...</h2>
-    const {animal, breed, city, state, description, name,} = this.state
+    const {animal, breed, city, state, description, name, images} = this.state
     return (
       <div className="details">
+        <Carousel images = {images}/>
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>

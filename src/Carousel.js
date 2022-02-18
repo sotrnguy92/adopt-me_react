5442d08216dacaff15151cfe33e2027d55d81c7e
sotrnguy92 <sonnguyen092@gmail.com>
2022-02-17 22:21:25 -0800

    adding modal component using portals

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,23 +1,23 @@
 import { Component } from "react";
 
-class Carousel extends Component{
+class Carousel extends Component {
   state = {
-    active: 0
+    active: 0,
   };
 
   static defaultProps = {
-    images: ['http://pets-images.dev-apis.com/pets/none.jpg']
-  }
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
 
   //with arrow functions the "this" refers to "this" where ever it is called instead of where it is created
   handleIndexClick = (e) => {
     this.setState({
-      active: +e.target.dataset.index
-    })
-  }
-  render(){
-    const {active} = this.state;
-    const {images} =this.props;
+      active: +e.target.dataset.index,
+    });
+  };
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
 
     return (
       <div className="carousel">
@@ -25,19 +25,18 @@ class Carousel extends Component{
         <div className="carousel-smaller">
           {images.map((photo, index) => (
             <img
-              key = {photo}
-              src ={photo}
+              key={photo}
+              src={photo}
               data-index={index}
-              onClick = {this.handleIndexClick}
-              className = {index === active ? "active" : ""}
-              alt = "animal thumbnail"
+              onClick={this.handleIndexClick}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
             />
           ))}
         </div>
       </div>
-    )
+    );
   }
-  
 }
 
-export default Carousel
+export default Carousel;
--- src/Details.js
+++ src/Details.js
@@ -2,9 +2,11 @@ import { Component } from "react";
 import { withRouter } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
+import Modal from "./Modal";
 
 class Details extends Component {
-  state = { loading: true };
+  state = { loading: true , showModal: false};
 
   async componentDidMount() {
     const res = await fetch(
@@ -21,6 +23,9 @@ class Details extends Component {
     );
   }
 
+  toggleModal = ()=> this.setState({showModal: !this.state.showModal})
+  adopt = () => (window.location = 'http://bit.ly/pet-adopt')
+
   render() {
     if (this.state.loading) return <h2>loading...</h2>;
     const { animal, breed, city, state, description, name, images } =
@@ -32,9 +37,26 @@ class Details extends Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
-          <button>Adopt {name}</button>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button onClick={this.toggleModal} style={{ backgroundColor: theme }}>Adopt {name}</button>
+            )}
+          </ThemeContext.Consumer>
           <p>{description}</p>
         </div>
+        {
+          this.state.showModal ? (
+            <Modal>
+              <div>
+                <h2>Would you like to adopt {name}?</h2>
+                <div className="buttons">
+                  <button onClick={this.adopt}> Yes </button>
+                  <button onClick={this.toggleModal}> No </button>
+                </div>
+              </div>
+            </Modal>
+          ) : null
+        }
       </div>
     );
   }
